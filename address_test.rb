099a7eb38f50ab7ccea9a960115cf850539b5534
Daisuke Sakamoto <daisuke.sakamoto+github@mixi.co.jp>
2017-04-14 16:32:50 +0900

    テストケース追加

--- address_test.rb
+++ address_test.rb
@@ -3,7 +3,31 @@ require 'minitest/unit'
 require 'minitest/autorun'
 
 class TestAddress < MiniTest::Unit::TestCase
-  def test_hello
-    assert false
+  [
+    "abc@example.com",
+    "a.b.c@example.com",
+
+    '"abc"@example.com',
+
+    '" abc"@example.com',
+    '"abc "@example.com',
+    '" abc "@example.com',
+
+    "123@example.com",
+  ].each do |address|
+    define_method "test_valid_#{address}" do
+      assert validate_address(address), "#{address} is expected valid"
+    end
+  end
+
+  [
+    "a..bc@example.com",
+    "　a@example.com",
+    '" qcontent".@example.com',
+    '.abc@example.com'
+  ].each do |address|
+    define_method "test_invalid_#{address}" do
+      assert validate_address(address) == false, "#{address} is expected invalid"
+    end
   end
 end
