d34f8c62a34bf39aa8c8e6c3cb28e70d1238732d
Daisuke Sakamoto <daisuke.sakamoto+github@mixi.co.jp>
2017-04-14 17:15:56 +0900

    だいたい通った

--- address.rb
+++ address.rb
@@ -1,2 +1,43 @@
-class Address
+def validate_address(address)
+  addr_spec === address
+end
+
+def atext
+  /([a-zA-Z])|([0-9])|\!|\#|\$|\%|\&|\'|\*|\+|\-|\/|\=|\?|\^|\_|\`|\{|\}|\||\~/
+end
+
+def dot_atom
+  /(#{atext})+(\.(#{atext})+)*/
+end
+
+def quoted_string
+  /\"((#{fws})?#{qcontent})*(#{fws})?\"/
+end
+
+def fws
+  /\ /
+end
+
+def quoted_pair
+  /\\([a-zA-Z0-9\ ])/
+end
+
+def qtext
+  /([a-zA-Z])|([0-9])|\!|\#|\$|\%|\&|\'|\*|\+|\-|\/|\=|\?|\^|\_|\`|\{|\}|\||\~|\./
+end
+
+def qcontent
+  /(#{qtext})|(#{quoted_pair})/
+end
+
+def local_part
+  /(#{dot_atom})|(#{quoted_string})/
+end
+
+def domain
+  "example.com"
+end
+
+def addr_spec
+  /^(#{local_part})@(#{domain})$/
 end
--- address_test.rb
+++ address_test.rb
@@ -14,6 +14,13 @@ class TestAddress < MiniTest::Unit::TestCase
     '" abc "@example.com',
 
     "123@example.com",
+
+    "1.2.3@example.com",
+    '"1.2.3"@example.com',
+
+    "'@example.com",
+    "\"'\"@example.com",
+
   ].each do |address|
     define_method "test_valid_#{address}" do
       assert validate_address(address), "#{address} is expected valid"
@@ -23,11 +30,36 @@ class TestAddress < MiniTest::Unit::TestCase
   [
     "a..bc@example.com",
     "　a@example.com",
+    "b　a@example.com",
+    "b　@example.com",
     '" qcontent".@example.com',
-    '.abc@example.com'
+    '.abc@example.com',
+    "ab\\cd@example.com",
+    '"a""@example.com',
   ].each do |address|
     define_method "test_invalid_#{address}" do
       assert validate_address(address) == false, "#{address} is expected invalid"
+    end
+  end
+
+  [
+    "abc",
+    "a.b.c",
+    "ab.c"
+  ].each do |atom|
+    define_method "test_valid_atom_#{atom}" do
+      assert /^(#{dot_atom})$/ === atom, "atom: #{atom} is expected valid"
+    end
+  end
+
+  [
+    ".abc",
+    "abc.",
+    "a..b",
+    "ab..c"
+  ].each do |atom|
+    define_method "test_invalid_atom_#{atom}" do
+      assert (/^(#{dot_atom})$/ === atom) == false, "atom: #{atom} is expected invalid"
     end
   end
 end
